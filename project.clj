09fed7ae1c59bbb6841cbd80d386375b8e2acc6d
James Laver <james.laver@gmail.com>
2015-03-16 19:49:22 +0000

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oolong "0.1.0-SNAPSHOT"
+(defproject oolong "0.1.0"
   :description "A config-based loader for stuartsierra's `component` library"
   :url "http://github.com/jjl/"
   :license {:name "MIT License"
