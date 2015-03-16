0248048843b1a03bbd0c9a5f5d12473cd544e617
James Laver <james.laver@gmail.com>
2015-03-16 21:03:17 +0000

    project.clj

--- project.clj
+++ project.clj
@@ -2,17 +2,24 @@
   :description "A config-based loader for stuartsierra's `component` library"
   :url "http://github.com/jjl/"
   :license {:name "MIT License"
-            :url "https://en.wikipedia.org/wiki/MIT_License"}
+            :url "https://en.wikipedia.org/wiki/MIT_License"
+            :distribution :repo}
   :plugins [[lein-midje "3.1.3"]
             [codox "0.8.11"]]
-   :source-paths ["src"]
+  :source-paths ["src"]
   :test-paths ["t"]
-;  :target-path "target/%s"
   :clean-targets ^{:protect false} ["target"]
+  :deploy-repositories [["releases" :clojars]]
+  :release-tasks [["doc"]
+                  ["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.reader "0.8.16"]
-                 [com.stuartsierra/component "0.2.3"]
-                 [midje "1.6.3"]]
-;  :dev-dependencies [[midje "1.6.3"]]
-  :profiles {:dev { :test-paths ["t"]}})
+                 [com.stuartsierra/component "0.2.3"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+
 
